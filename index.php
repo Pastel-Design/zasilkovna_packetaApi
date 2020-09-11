8c61499afafcefab58ba1e22ec322c5a320b10c4
kukivac <50522557+kukivac@users.noreply.github.com>
2020-09-11 17:40:18 +0200

    initialization

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <button id="pickup">Vybrat pobočku</button>
+    <p id="branchInfo"></p>
+</body>
+
+</html>
+<script src="script.js"></script>
+<script>
+    let pickupButton = document.getElementById("pickup")
+    let branchInfo = document.getElementById("branchInfo")
+
+    function openWindowWithPost(url, data) {
+        var form = document.createElement("form");
+        form.target = "_blank";
+        form.method = "POST";
+        form.action = url;
+        form.style.display = "none";
+
+            var input = document.createElement("input");
+            input.type = "hidden";
+            input.name = "json";
+            input.value = data;
+            form.appendChild(input);
+
+        document.body.appendChild(form);
+        form.submit();
+        document.body.removeChild(form);
+    }
+
+    let pickBranch = function() {
+        Packeta.Widget.pick("1d829952011ea051", function(data) {
+            openWindowWithPost("pobocka.php", JSON.stringify(data))
+            console.log(data)
+        })
+    }
+    pickupButton.addEventListener("click", pickBranch)
+</script>
